275d01229ce6c004ef15af9f4f1ae9dd01eafb8d
Arun Roberts <arunplaysbass@gmail.com>
2022-07-15 19:42:28 +1000

    trialling subheader. styled together.

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,53 +3,57 @@ import styles from '../styles/pages/About.module.css'
 
 const About = () => {
     return (
-        <main className={styles.about}>
-            <h1 className={styles.about__title}>
-                May I tempt you for a spell? 
-            </h1>
-            <section className={styles.about_item}>
-                <figure className={styles.about_item_figure}>
-                    <Image 
-                        className={styles.about_figure__image}
-                        src='/assets/Yasmin1.jpg'
-                        layout='fill'
-                        objectFit='contain'
-                        alt='Yasmin'
-                    />
-                </figure>
-                <div className={styles.about_item__body}>
-                    Don’t be fooled by my petite stature, my cheeky grin is utterly luminous and my delicious curves… impactful.  Some  have even described me as intoxicating. Time stands still when you’re luxuriating in the warmth and guidance of my sweet, nurturing and thoughtful companionship. Pure unadulterated bliss.
-                </div>
+        <>
+            <section className={styles.about_title}>
+                <h1 className={styles.about_title__text}>
+                    May I tempt you for a spell? 
+                </h1>
             </section>
-            <section className={styles.about_item}>
-                <figure className={styles.about_item_figure}>
-                    <Image 
-                        className={styles.about_figure__image}
-                        src='/assets/Yasmin2.jpg'
-                        layout='fill'
-                        objectFit='contain'
-                        alt='Yasmin'
-                    />
-                </figure>
-                <div className={styles.about_item__body}>
-                    My mesmerising figure is kept in shape with joyous dance. Not your gym shark fitness guru type, I am down to earth yet indulgent. I inject passion into all pursuits, be it social justice, gardening or pleasure. I love Japanese whiskey, red wine, and fine dining dates as much as I adore dancing around the kitchen creating something nourishing and flavoured with lashings of love (and likely one of my homemade ferments). I yearn for connections that build over time and allow us to create new experiences together. An enthusiast for travel, art, vintage styles, documentaries, maths, science and the culinary arts we’ll never run out of things to share. 
-                </div>
-            </section>
-            <section className={styles.about_item}>
-                <figure className={styles.about_item_figure}>
-                    <Image 
-                        className={styles.about_figure__image}
-                        src='/assets/Yasmin3.jpg'
-                        layout='fill'
-                        objectFit='contain'
-                        alt='Yasmin'
-                    />
-                </figure>
-                <div className={styles.about_item__body}>
-                    The depth and variety  of human sexual expressions  does not easily shock me. Being entrusted  to create a safe space for lovers, to be a steady anchor in the sea of exploration is an honour and a privilege.  All desires, whether sweet and simple or dark and depraved, and all lovers from the more nervous novice or to the experienced adventurer, are worthy of care and attention. Nothing brings me more joy than to curate an experience where lovers can truly relax, release, revel in and receive the endless iterations of their erotic potential. Your secret is safe with me. 
-                </div>
-            </section>
-        </main>
+            <main className={styles.about}>
+                <section className={styles.about_item}>
+                    <figure className={styles.about_item_figure}>
+                        <Image 
+                            className={styles.about_figure__image}
+                            src='/assets/Yasmin1.jpg'
+                            layout='fill'
+                            objectFit='contain'
+                            alt='Yasmin'
+                        />
+                    </figure>
+                    <div className={styles.about_item__body}>
+                        Don’t be fooled by my petite stature, my cheeky grin is utterly luminous and my delicious curves… impactful.  Some  have even described me as intoxicating. Time stands still when you’re luxuriating in the warmth and guidance of my sweet, nurturing and thoughtful companionship. Pure unadulterated bliss.
+                    </div>
+                </section>
+                <section className={styles.about_item}>
+                    <figure className={styles.about_item_figure}>
+                        <Image 
+                            className={styles.about_figure__image}
+                            src='/assets/Yasmin2.jpg'
+                            layout='fill'
+                            objectFit='contain'
+                            alt='Yasmin'
+                        />
+                    </figure>
+                    <div className={styles.about_item__body}>
+                        My mesmerising figure is kept in shape with joyous dance. Not your gym shark fitness guru type, I am down to earth yet indulgent. I inject passion into all pursuits, be it social justice, gardening or pleasure. I love Japanese whiskey, red wine, and fine dining dates as much as I adore dancing around the kitchen creating something nourishing and flavoured with lashings of love (and likely one of my homemade ferments). I yearn for connections that build over time and allow us to create new experiences together. An enthusiast for travel, art, vintage styles, documentaries, maths, science and the culinary arts we’ll never run out of things to share. 
+                    </div>
+                </section>
+                <section className={styles.about_item}>
+                    <figure className={styles.about_item_figure}>
+                        <Image 
+                            className={styles.about_figure__image}
+                            src='/assets/Yasmin3.jpg'
+                            layout='fill'
+                            objectFit='contain'
+                            alt='Yasmin'
+                        />
+                    </figure>
+                    <div className={styles.about_item__body}>
+                        The depth and variety  of human sexual expressions  does not easily shock me. Being entrusted  to create a safe space for lovers, to be a steady anchor in the sea of exploration is an honour and a privilege.  All desires, whether sweet and simple or dark and depraved, and all lovers from the more nervous novice or to the experienced adventurer, are worthy of care and attention. Nothing brings me more joy than to curate an experience where lovers can truly relax, release, revel in and receive the endless iterations of their erotic potential. Your secret is safe with me. 
+                    </div>
+                </section>
+            </main>
+        </>
     )
 }
 
--- pages/together.tsx
+++ pages/together.tsx
@@ -1,70 +1,117 @@
+import Image from 'next/image'
 import styles from '../styles/pages/Together.module.css'
 
 const Together = () => {
     return (
-        <main className={styles.together}>
-            <h1>Pleasure menu</h1>
-            <section>
-                <h2>Social</h2>
-                <p>$300 p/h</p>
-                <p>
-                    Let’s get to know each other over lunch or dinner, at one of my favourite spots in Melbourne.
-                </p>
+        <>
+            <section className={styles.subheader}>
+                <h1 className={styles.subheader__title}>Pleasure menu</h1>
             </section>
-            <section>
-                <h2>Salacious</h2>
-                <ul>
-                    <li>1 hour: <span>$600</span></li>
-                    <li>2 hours: <span>$1100</span></li>
-                    <li>3 hours: <span>$1600</span></li>
-                </ul>
-                <p>Time spent together at a 4 or 5 star hotel in the CBD and inner suburbs.</p>
-            </section>
-            <section>
-                <h2>Mixed</h2>
-                <p>
-                    The time we will spend together will be equally split between being social and salacious. I love planning dates and always have a few ideas in mind. Get in touch and based on our mutual interests, together we’ll work out the perfect date.
-                </p>
-                <dl>
-                    <dt>4 hour date</dt>
-                    <dd>$2000</dd>
-                    <dt>6 hour date</dt>
-                    <dd>$2600</dd>
-                </dl>
-            </section>
-            <section>
-                <h2>Foodie date</h2>
-                <p>
-                    Cooking and sharing a home cooked meal are two of the greatest pleasures in life to me. During the first half of our session, I’ll whip up a meal for the two of us to enjoy while you sit back, relax and sip on a glass of whiskey. Or perhaps you’d like to join me in the kitchen?
-                </p>
-                <p>
-                    With our stomachs and souls nourished with our creation, we can indulge in the bedroom during the second half of our time together.
-                </p>
-                <p>
-                    When getting in contact, please include any allergies, dietary requirements, likes and dislikes so I can curate the perfect menu for us.
-                </p>
-                <p>
-                    Please note that I required a week’s notice for the Foodie Date experience due to the amount of time involved in planning the menu and purchasing the ingredients.
-                </p>
-                <dl>
-                    <dt>4 hours</dt>
-                    <dd>$2400</dd>
-                    <dt>6 hours</dt>
-                    <dd>$2800</dd>
-                </dl>
-            </section>
-            <section>
-                <h3>Overnight</h3>
-                <p>$4000</p>
-                <p>15 hours with a minimum of 7 hours of sleep</p>
-            </section>
-            <aside>
-                Weekends away, adventures, travel companion - please contact me to discuss and arrange.
-            </aside>
-            <aside>
-                The above rates are inclusive of travel within a 5km radius of the CBD.
-            </aside>
-        </main>
+            <main className={styles.together}>
+                <section className={styles.together_item}>
+                    <figure className={styles.together_item__image}>
+                        <Image
+                            src='/assets/Yasmin7.jpg'
+                            layout='fill'
+                            objectFit='cover'
+                            alt='Yasmin'
+                        />
+                    </figure>
+                    <div className={styles.together_item_text}>
+                        <h2>Social</h2>
+                        <p>$300 p/h</p>
+                        <p>
+                            Let’s get to know each other over lunch or dinner, at one of my favourite spots in Melbourne.
+                        </p>
+                    </div>
+                </section>
+                <section className={styles.together_item}>
+                    <figure className={styles.together_item__image}>
+                        <Image
+                            src='/assets/Yasmin6.jpg'
+                            layout='fill'
+                            objectFit='cover'
+                            alt='Yasmin'
+                        />
+                    </figure>
+                    <div className={styles.together_item_text}>
+                        <h2>Salacious</h2>
+                        <ul>
+                            <li>1 hour: <span>$600</span></li>
+                            <li>2 hours: <span>$1100</span></li>
+                            <li>3 hours: <span>$1600</span></li>
+                        </ul>
+                        <p>
+                            Time spent together at a 4 or 5 star hotel in the CBD and inner suburbs.
+                        </p>
+                    </div>
+                </section>
+                <section className={styles.together_item}>
+                    <figure className={styles.together_item__image}>
+                        <Image
+                            src='/assets/Yasmin8.jpg'
+                            layout='fill'
+                            objectFit='cover'
+                            alt='Yasmin'
+                        />
+                    </figure>
+                    <div className={styles.together_item_text}>
+                        <h2>Mixed</h2>
+                        <p>
+                            The time we will spend together will be equally split between being social and salacious. I love planning dates and always have a few ideas in mind. Get in touch and based on our mutual interests, together we’ll work out the perfect date.
+                        </p>
+                        <dl>
+                            <dt>4 hour date</dt>
+                            <dd>$2000</dd>
+                            <dt>6 hour date</dt>
+                            <dd>$2600</dd>
+                        </dl>
+                    </div>
+                </section>
+                <section className={styles.together_item}>
+                    <figure className={styles.together_item__image}>
+                        <Image
+                            src='/assets/Yasmin11.jpg'
+                            layout='fill'
+                            objectFit='cover'
+                            alt='Yasmin'
+                        />
+                    </figure>
+                    <div className={styles.together_item_text}>
+                        <h2>Foodie date</h2>
+                        <p>
+                            Cooking and sharing a home cooked meal are two of the greatest pleasures in life to me. During the first half of our session, I’ll whip up a meal for the two of us to enjoy while you sit back, relax and sip on a glass of whiskey. Or perhaps you’d like to join me in the kitchen?
+                        </p>
+                        <p>
+                            With our stomachs and souls nourished with our creation, we can indulge in the bedroom during the second half of our time together.
+                        </p>
+                        <p>
+                            When getting in contact, please include any allergies, dietary requirements, likes and dislikes so I can curate the perfect menu for us.
+                        </p>
+                        <p>
+                            Please note that I required a week’s notice for the Foodie Date experience due to the amount of time involved in planning the menu and purchasing the ingredients.
+                        </p>
+                        <dl>
+                            <dt>4 hours</dt>
+                            <dd>$2400</dd>
+                            <dt>6 hours</dt>
+                            <dd>$2800</dd>
+                        </dl>
+                    </div>
+                </section>
+                <section>
+                    <h3>Overnight</h3>
+                    <p>$4000</p>
+                    <p>15 hours with a minimum of 7 hours of sleep</p>
+                </section>
+                <aside>
+                    Weekends away, adventures, travel companion - please contact me to discuss and arrange.
+                </aside>
+                <aside>
+                    The above rates are inclusive of travel within a 5km radius of the CBD.
+                </aside>
+            </main>
+        </>
     )
 }
 
