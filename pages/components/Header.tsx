320e2c479c4846fdff78917cfc08e6c775f74f79
Arun Roberts <arunplaysbass@gmail.com>
2022-08-28 14:30:18 +1000

    added slide to nav for mobile

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -1,17 +1,22 @@
 import Nav from './Nav'
 import Link from 'next/link'
+import { useState } from 'react'
 import styles from '../../styles/components/Header.module.css'
 
 const Header = () => {
+    const [ dropDown, setDropDown ] = useState(false)
     return (
         <header className={styles.header}>
             <Link href='/'>
                 <a className={styles.header__title}>YASMIN HANA</a>
             </Link>
-            <div className={styles.header__nav}>
+            <div 
+                className={dropDown ? `${styles.header__nav} ${styles.header__nav___open}` : `${styles.header__nav}`}
+                onClick={() => setDropDown(false)} 
+            >
                 <Nav />
             </div>
-            <div className={styles.header_mobileOnly}>
+            <div onClick={() => setDropDown(!dropDown)} className={styles.header_mobileOnly}>
                 <div className={styles.header_mobileOnly__item}></div>
                 <div className={styles.header_mobileOnly__item}></div>
                 <div className={styles.header_mobileOnly__item}></div>
